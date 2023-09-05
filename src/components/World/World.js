4f4e1d053252f3d89fb6eb680371d854230287f7
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-09-05 16:01:55 +0500

    [Build: Make buttons unique for every country]

--- src/components/World/World.js
+++ src/components/World/World.js
@@ -17,81 +17,81 @@ const World = () => {
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="chinese-button">
                 Chinese <img className="arabic-flag" src={Arabic} alt="" />
                 中国人
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className=" dutch-button">
                 Dutch <img className="arabic-flag" src={Arabic} alt="" />Dutch
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="english-button arabic-button">
                 English <img className="arabic-flag" src={Arabic} alt="" />
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="french-button arabic-button">
                 French <img className="arabic-flag" src={Arabic} alt="" />
                 Français
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="german-button arabic-button">
                 German <img className="arabic-flag" src={Arabic} alt="" />
                 Deutsch
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="habrew-button arabic-button">
                 Habrew <img className="arabic-flag" src={Arabic} alt="" />
                 العربية
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="hindi-button arabic-button">
                 Hindi <img className="arabic-flag" src={Arabic} alt="" />
                 हिंदी
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="italian-button arabic-button">
                 Italian <img className="arabic-flag" src={Arabic} alt="" />
                 Italiano
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="malyalam-button arabic-button">
                 Malayalam <img className="arabic-flag" src={Arabic} alt="" />
                 മലയാളം
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="polish-button arabic-button">
                 Polish <img className="arabic-flag" src={Arabic} alt="" />{" "}
                 POLSKI
               </button>
             </div>
             <div>
-              <button className="arabic-button">
+              <button className="portugees-button arabic-button">
                 Portugees <img className="arabic-flag" src={Arabic} alt="" />{" "}
                 português
               </button>
-              <button className="arabic-button">
+              <button className="russian-button arabic-button">
                 Russian <img className="arabic-flag" src={Arabic} alt="" />{" "}
                 Русский
               </button>
-              <button className="arabic-button">
-                Sinhala <img className="arabic-flag" src={Arabic} alt="" />{" "}
+              <button className="sinhala-button arabic-button">
+                Sinhala <img className=" arabic-flag" src={Arabic} alt="" />{" "}
                 සිංහල
               </button>
-              <button className="arabic-button">
-                Spanish <img className="arabic-flag" src={Arabic} alt="" />{" "}
+              <button className="spanish-button arabic-button">
+                Spanish <img className=" arabic-flag" src={Arabic} alt="" />{" "}
                 Española
               </button>
-              <button className="arabic-button">
+              <button className="ukranian-button arabic-button">
                 Ukranian <img className="arabic-flag" src={Arabic} alt="" />{" "}
                 українська
               </button>
